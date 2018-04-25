cf5f674051f6ea0f70673e06d37e600afa9df4d0
Joerg Kuetemeier <jk@kuetemeier.net>
2018-04-25 20:06:13 +0200

    Add Plugin class.

---
+++ src/Kuetemeier/WordPress/Plugin.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4:
+ *
+ * @package    kuetemeier-essentials
+ * @author     Jörg Kütemeier (https://kuetemeier.de/kontakt)
+ * @license    GNU General Public License 3
+ * @link       https://kuetemeier.de
+ * @copyright  2018 Jörg Kütemeier
+ *
+ *
+ * Copyright 2018 Jörg Kütemeier (https://kuetemeier.de/kontakt)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace Kuetemeier\WordPress;
+
+/*********************************
+ * KEEP THIS for security reasons
+ * blocking direct access to our plugin PHP files by checking for the ABSPATH constant
+ */
+defined( 'ABSPATH' ) || die( 'No direct call!' );
+
+
+class Plugin {
+
+}

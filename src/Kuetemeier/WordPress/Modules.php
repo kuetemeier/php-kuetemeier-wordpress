152288c0f47fd2dba9055512b83af2a43a29e1fb
Joerg Kuetemeier <jk@kuetemeier.net>
2018-04-26 15:59:57 +0200

    Add some priorty features.

--- src/Kuetemeier/WordPress/Modules.php
+++ src/Kuetemeier/WordPress/Modules.php
@@ -36,41 +36,98 @@ defined( 'ABSPATH' ) || die( 'No direct call!' );
 
 class Modules extends \Kuetemeier\Collection\Collection {
 
+    /**
+     * Holds a list of module IDs to preserve order.
+     *
+     * @see Modules::load_sources() Populated by Modules::load_sources().
+     * @see Modules::init_module_classes() Used by Modules::init_module_classes().
+     */
+    private $id_list = array();
+
+
+    /**
+     * Reference to the plugin configuration.
+     *
+     * @see Config
+     */
     private $config;
 
+
+    /**
+     * Creates a Modules managing instance.
+     *
+     * @param Config $config Reference to the plugin configuartion.
+     */
     public function __construct($config) {
         $this->config = $config;
     }
 
+    /**
+     * Load the php source file of all modules listed in `$modules_list` and adds their
+     * primary class name to the `$this->class_list`.
+     *
+     * The source files (and so the list of classes in `$this->class_list`) are loaded
+     * in the order that is determined by the priorty in the config `plugin/moudles/available`
+     * hash. NOTICE: So a module MUST be in this list to be loaded.
+     *
+     * @param string[] $modules_list Array with module IDs to be loaded.
+     */
     public function load_sources($modules_list = array()) {
-        if (!isset($modules_list) || count($modules_list) === 0) {
+        // prevent a second call of this function
+        if (!empty($this->elements)) {
+            wp_die("Modules::load_sources: ERROR - Elements of Modules are not empty. Do you try to load_sources twice?");
+        }
+
+        // don't do anything if list is empty
+        if (!isset($modules_list) || empty($modules_list)) {
             return;
         }
 
+        // get all available modules
+        $all_modules = $this->config->get('plugin/modules/available', array());
+
+        if (empty($all_modules)) {
+            return;
+        }
+
+        // sort $all_modules by priority
+        uksort($all_modules, function($a, $b) {
+            return (int) $a - (int) $b;
+        });
+
+        // prepare module namespace
         $namespace = '\\'.$this->config->get('plugin/modules/namespace').'\\';
 
-        foreach($modules_list as $module_id) {
-            $srcdir = trailingslashit($this->config->get('plugin/modules/srcdir', trailingslashit($this->config->get('plugin/dir')).'src/module'));
+        // iterate over all available modules in priority order
+        foreach($all_modules as $module_id) {
+            // and load php source, if it is in the $modules_list
+            if (isset($modules_list[$module_id])) {
+                $srcdir = trailingslashit($this->config->get('plugin/modules/srcdir', trailingslashit($this->config->get('plugin/dir')).'src/module'));
 
-            require_once $srcdir.'class-'.$module_id.'.php';
+                require_once $srcdir.'class-'.$module_id.'.php';
 
-            $class_name = $namespace.ucfirst($module_id);
+                $class_name = $namespace.ucfirst($module_id);
 
-            $manifest = $class_name::manifest();
+                $manifest = $class_name::manifest();
 
-            $this->config->set('default/'.$module_id, $manifest['config'], true);
+                $this->config->set('default/'.$module_id, $manifest['config'], true);
 
-            $this->set($module_id, $class_name);
+                array_push($this->elements, $module_id, $class_name);
+            }
         }
     }
 
     public function init() {
 
         // get all modules that come with this plugin (default: none - empty array)
-        $all_modules = $this->config->get('plugin/modules/list', array());
+        $all_modules     = array_keys($this->config->get('plugin/modules/available', array()));
+        $default_enabled = $this->config->get('plugin/modules/default-enabled', array());
+        $always_enabled  = $this->config->get('plugin/modules/always-enabled', array());
 
         // load only activated modules (with fallback to all) if this is a frontend call
-        $modules_list = (is_admin()) ? $all_modules : $this->config->get('options/modules/activated', $all_modules);
+        $modules_list = (is_admin()) ? $all_modules : $this->config->get('options/modules/enabled', $default_enabled);
+
+        $modules_list = array_unique(array_merge($modules_list, $always_enabled));
 
         $this->load_sources($modules_list);
     }
